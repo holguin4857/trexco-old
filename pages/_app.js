877065fbf6d9e782ef527a3da47c070378790778
holguin4857 <omar@trexco.net>
2023-02-13 15:29:24 +0100

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '@/styles/globals.css'
+import '@/styles/navbar.css'
+import '@/styles/utilities.css'
+import '@/styles/footer.css'
+
+
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
