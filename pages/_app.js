514f7b6a4149f7caaf653b925dda0e476602c36e
holguin4857 <omar@trexco.net>
2023-02-14 15:40:53 +0100

    using next/font

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,20 @@ import '@/styles/globals.css'
 import '@/styles/navbar.css'
 import '@/styles/utilities.css'
 import '@/styles/footer.css'
+import { Poppins } from '@next/font/google'
+
+const poppins = Poppins({ 
+  subsets: ['latin'],
+  weight: ['400', '700'],
+ });
 
 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <main className={poppins.className}>
+       <Component {...pageProps} />
+     </main>
+  );
 }
