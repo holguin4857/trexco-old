b47539acae1f587c3f331cb147843a39f0c4d4e3
holguin4857 <omar@trexco.net>
2023-02-14 15:48:54 +0100

    font attempt

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import { Poppins } from '@next/font/google'
 
 const poppins = Poppins({ 
   subsets: ['latin'],
-  weight: ['400', '700'],
+  weight: ['400'],
  });
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import Navbar from "@/components/Navbar";
 import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
